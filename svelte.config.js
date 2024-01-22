62f9eab59399cce74b74885008841a877c6cda63
AnassBousseaden <anassbousseaden@yahoo.fr>
2024-01-22 18:13:09 +0100

    te

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
+import {
+	vitePreprocess
+} from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/PathFinding' : '',
 		}
 	}
 };
-
 export default config;
-
