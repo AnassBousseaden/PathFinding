799ef36f57ef69999b8bbadca64a32607839e950
AnassBousseaden <anassbousseaden@yahoo.fr>
2024-01-22 18:37:26 +0100

    change title and icon

--- build/_app/immutable/entry/start.ArnlbRRW.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.dPHnm2xE.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
-import {
-	vitePreprocess
-} from '@sveltejs/kit/vite';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
